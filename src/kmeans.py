28302f19885b21e64a2803f6cd0daf99cbd4d6d2
Mohamed Adnan Azmie <u7601545@anu.edu.au>
2024-04-28 18:07:41 +1000

    completed kmeans algorthim using class system

---
+++ src/kmeans.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class KMeans:
+    def __init__(self, n_clusters, max_iter=100):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+
+    def fit(self, X):
+        # Randomly initialize centroids
+        centroids = X[np.random.choice(X.shape[0], self.n_clusters, replace=False)]
+        
+        for _ in range(self.max_iter):
+            # Assign each data point to the nearest centroid
+            labels = np.argmin(((X[:, None] - centroids)**2).sum(axis=2), axis=1)
+            
+            # Update centroids by taking the mean of all data points assigned to each centroid
+            new_centroids = np.array([X[labels == k].mean(axis=0) for k in range(self.n_clusters)])
+            
+            # Check for convergence
+            if np.allclose(new_centroids, centroids):
+                break
+            
+            centroids = new_centroids
+        
+        self.labels_ = labels
+        self.cluster_centers_ = centroids
