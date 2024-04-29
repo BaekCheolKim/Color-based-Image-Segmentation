7c4b9d318e5b2f83e81ddbc5b638fe10f89beee3
Mohamed Adnan Azmie <u7601545@anu.edu.au>
2024-04-29 20:17:19 +1000

    Inclusion of tolerance to improve kmeans algorithm

--- src/kmeans.py
+++ src/kmeans.py
@@ -5,13 +5,26 @@ class KMeans:
     """
     Display segmented image: segmented_img = centroids[labels].reshape(img.shape)
     """
-    def __init__(self, n_clusters, max_iter=100):
-        """Initialise k means algorithm with required number of clusters and maximum interations"""
+    def __init__(self, n_clusters, max_iter=100, atol=1e-4):
+        """
+        Initialize KMeans clustering algorithm.
+        
+        Parameters:
+        - n_clusters: int, number of clusters (k)
+        - max_iter: int, maximum number of iterations
+        - atol: float, tolerance to declare convergence
+        """
         self.n_clusters = n_clusters
         self.max_iter = max_iter
+        self.atol = atol
 
     def fit(self, X):
-        """Perform K means algorithm for given n_cluster on float 64 image normalised and reshaped into single vector"""
+        """
+        Fit KMeans to the data.
+        
+        Parameters:
+        - X: array-like, shape (n_samples, n_features), input data
+        """
         # Randomly initialize centroids
         centroids = X[np.random.choice(X.shape[0], self.n_clusters, replace=False)]
         for _ in range(self.max_iter):
@@ -20,7 +33,7 @@ class KMeans:
             # Update centroids by taking the mean of all data points assigned to each centroid
             new_centroids = np.array([X[labels == k].mean(axis=0) for k in range(self.n_clusters)])
             # Check for convergence
-            if np.allclose(new_centroids, centroids):
+            if np.allclose(new_centroids, centroids, atol = self.atol):
                 break
             centroids = new_centroids
         self.labels_ = labels
