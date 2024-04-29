498bf3562fbf49789c02a1a42b555a61784ca6fd
Mohamed Adnan Azmie <u7601545@anu.edu.au>
2024-04-29 16:33:19 +1000

    Few minor changes

--- tests/test_kmeans.py
+++ tests/test_kmeans.py
@@ -8,14 +8,13 @@ extract_path = 'data'
 with tarfile.open(tar_path_cifar10, "r:gz") as tar:
     tar.extractall(path=extract_path)
 
-image = None
-image = np.array(image, dtype=np.float64) / 255
-pixels = image.reshape((-1, 3))
-cluster = 5
+image = None  # Add Cifar10 image here
+iamge = (np.array(image, dtype=np.float64) / 255).reshape((-1,3))
+cluster = 4
 
 # Perform K-means clustering
-Kmeans = kmeans.KMeans(n_clusters=cluster)
-Kmeans.fit(pixels)
+Kmeans = kmeans.KMeans(n_clusters=cluster, max_iter=200)
+Kmeans.fit(image)
 labels = Kmeans.labels_
 centroids = Kmeans.cluster_centers_
 
